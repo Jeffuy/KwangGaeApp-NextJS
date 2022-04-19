a2caca783a04e629e5f0907e75ec0edc70e17d7d
Jeff Free <jefffreelance3@gmail.com>
2022-04-19 14:14:32 -0300

    :lipstick: Fight UI updated

--- src/components/fight/FigthButtons.jsx
+++ src/components/fight/FigthButtons.jsx
@@ -6,111 +6,112 @@ const FigthButtons = () => {
 	const { status, setShowError, lastRed, lastBlue, startFight, endFight, redScore, blueScore, redWarning, blueWarning, timerOn, pauseFight, time, resumeFight } = useContext(FightContext);
 
 	return (
-		<div className="container">
-			<div className="row">
-				<div className="col-6 mb-2">
-					<div className="btn-group-vertical">
-						<button
-							className="btn btn-danger btn-lg btn-block mt-3 mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? redScore(1) : setShowError(true);
-							}}
-						>
-							Golpe de puño
-						</button>
-						<button
-							className="btn btn-danger btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? redScore(2) : setShowError(true);
-							}}
-						>
-							Patada zona media
-						</button>
-						<button
-							className="btn btn-danger btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? redScore(3) : setShowError(true);
-							}}
-						>
-							Patada zona alta
-						</button>
-						<button
-							className="btn btn-danger btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? redWarning() : setShowError(true);
-							}}
-						>
-							Advertencia
-						</button>
-						<button
-							className="btn btn-danger btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? redScore(-1) : setShowError(true);
-							}}
-						>
-							Punto en contra
-						</button>
-						<button className="btn btn-danger btn-lg btn-block mb-2 shadow-none" type="button" onClick={() => lastRed(status)}>
-							Deshacer último
-						</button>
-					</div>
+		<>
+			<div className="container d-flex justify-content-around">
+				<div className="d-flex flex-column justify-content-around m-2 ">
+					<button
+						className="btn btn-danger btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? redScore(1) : setShowError(true);
+						}}
+					>
+						1 Punto
+					</button>
+
+					<button
+						className="btn btn-danger btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? redScore(2) : setShowError(true);
+						}}
+					>
+						2 Puntos
+					</button>
+
+					<button
+						className="btn btn-danger btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? redScore(3) : setShowError(true);
+						}}
+					>
+						3 Puntos
+					</button>
+					<button
+						className="btn btn-danger btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? redWarning() : setShowError(true);
+						}}
+					>
+						Advertencia
+					</button>
+					<button
+						className="btn btn-danger btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? redScore(-1) : setShowError(true);
+						}}
+					>
+						-1 Punto
+					</button>
+					<button className="btn btn-danger btn-lg mb-4" type="button" onClick={() => lastRed(status)}>
+						Deshacer
+					</button>
 				</div>
-				<div className="col-6 mb-2">
-					<div className="btn-group-vertical">
-						<button
-							className="btn btn-primary btn-lg btn-block mt-3 mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? blueScore(1) : setShowError(true);
-							}}
-						>
-							Golpe de puño
-						</button>
-						<button
-							className="btn btn-primary btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? blueScore(2) : setShowError(true);
-							}}
-						>
-							Patada zona media
-						</button>
-						<button
-							className="btn btn-primary btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? blueScore(3) : setShowError(true);
-							}}
-						>
-							Patada zona alta
-						</button>
-						<button
-							className="btn btn-primary btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? blueWarning() : setShowError(true);
-							}}
-						>
-							Advertencia
-						</button>
-						<button
-							className="btn btn-primary btn-lg btn-block mb-2 shadow-none"
-							type="button"
-							onClick={() => {
-								status ? blueScore(-1) : setShowError(true);
-							}}
-						>
-							Punto en contra
-						</button>
-						<button className="btn btn-primary btn-lg btn-block mb-2 shadow-none" type="button" onClick={() => lastBlue(status)}>
-							Deshacer último
-						</button>
-					</div>
+
+				{/* // EMPIEZA AZUL */}
+
+				<div className="d-flex flex-column justify-content-around m-2">
+					<button
+						className="btn btn-primary btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? blueScore(1) : setShowError(true);
+						}}
+					>
+						1 Punto
+					</button>
+					<button
+						className="btn btn-primary btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? blueScore(2) : setShowError(true);
+						}}
+					>
+						2 Puntos
+					</button>
+					<button
+						className="btn btn-primary btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? blueScore(3) : setShowError(true);
+						}}
+					>
+						3 Puntos
+					</button>
+					<button
+						className="btn btn-primary btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? blueWarning() : setShowError(true);
+						}}
+					>
+						Advertencia
+					</button>
+					<button
+						className="btn btn-primary btn-lg mb-4"
+						type="button"
+						onClick={() => {
+							status ? blueScore(-1) : setShowError(true);
+						}}
+					>
+						-1 Punto
+					</button>
+					<button className="btn btn-primary btn-lg mb-4" type="button" onClick={() => lastBlue(status)}>
+						Deshacer
+					</button>
 				</div>
 			</div>
 			<FightTime />
@@ -138,7 +139,7 @@ const FigthButtons = () => {
 					</>
 				)}
 			</div>
-		</div>
+		</>
 	);
 };
 
