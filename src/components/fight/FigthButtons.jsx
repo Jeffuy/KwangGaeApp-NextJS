14c2d7916c17859c7d0aba3418d8feed0c058e7a
Jeff Free <jefffreelance3@gmail.com>
2022-04-20 15:16:04 -0300

    :lipstick: buttons mode choice on fight

--- src/components/fight/FigthButtons.jsx
+++ src/components/fight/FigthButtons.jsx
@@ -1,122 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { FightContext } from '@context/FightContext';
+import FightButtonsPro from '@components/fight/FightButtonsPro';
+import FightButtonsNoob from '@components/fight/FightButtonsNoob';
 import FightTime from '@components/fight/FightTime';
 
 const FigthButtons = () => {
-	const { status, setShowError, lastRed, lastBlue, startFight, endFight, redScore, blueScore, redWarning, blueWarning, timerOn, pauseFight, time, resumeFight } = useContext(FightContext);
+	const { status, startFight, endFight, timerOn, pauseFight, time, resumeFight } = useContext(FightContext);
+
+	const [pro, setPro] = useState(false);
 
 	return (
 		<>
-			<div className="container d-flex justify-content-around">
-				<div className="d-flex flex-column justify-content-around m-2 ">
-					<button
-						className="btn btn-danger btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? redScore(1) : setShowError(true);
-						}}
-					>
-						1 Punto
-					</button>
-
-					<button
-						className="btn btn-danger btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? redScore(2) : setShowError(true);
-						}}
-					>
-						2 Puntos
-					</button>
-
-					<button
-						className="btn btn-danger btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? redScore(3) : setShowError(true);
-						}}
-					>
-						3 Puntos
-					</button>
-					<button
-						className="btn btn-danger btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? redWarning() : setShowError(true);
-						}}
-					>
-						Advertencia
-					</button>
-					<button
-						className="btn btn-danger btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? redScore(-1) : setShowError(true);
-						}}
-					>
-						-1 Punto
-					</button>
-					<button className="btn btn-danger btn-lg mb-4" type="button" onClick={() => lastRed(status)}>
-						Deshacer
-					</button>
-				</div>
-
-				{/* // EMPIEZA AZUL */}
-
-				<div className="d-flex flex-column justify-content-around m-2">
-					<button
-						className="btn btn-primary btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? blueScore(1) : setShowError(true);
-						}}
-					>
-						1 Punto
-					</button>
-					<button
-						className="btn btn-primary btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? blueScore(2) : setShowError(true);
-						}}
-					>
-						2 Puntos
-					</button>
-					<button
-						className="btn btn-primary btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? blueScore(3) : setShowError(true);
-						}}
-					>
-						3 Puntos
-					</button>
-					<button
-						className="btn btn-primary btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? blueWarning() : setShowError(true);
-						}}
-					>
-						Advertencia
-					</button>
-					<button
-						className="btn btn-primary btn-lg mb-4"
-						type="button"
-						onClick={() => {
-							status ? blueScore(-1) : setShowError(true);
-						}}
-					>
-						-1 Punto
-					</button>
-					<button className="btn btn-primary btn-lg mb-4" type="button" onClick={() => lastBlue(status)}>
-						Deshacer
-					</button>
-				</div>
-			</div>
+			{pro && <FightButtonsPro />}
+			{!pro && <FightButtonsNoob />}
 			<FightTime />
 			<div className="d-flex justify-content-center">
-				{!timerOn && time === 0 && (
+				{!timerOn && time < 20 && (
 					<button className="btn btn-dark btn-lg mt-1 mb-3" type="button" onClick={status ? undefined : () => startFight()}>
 						<i className="fa fa-play" />
 					</button>
@@ -131,13 +30,24 @@ const FigthButtons = () => {
 						</button>
 					</>
 				)}
-				{!timerOn && time !== 0 && (
+				{!timerOn && time > 10 && (
 					<>
 						<button className="btn btn-dark btn-lg mt-1 mb-3" onClick={() => resumeFight()}>
 							Resume
 						</button>
 					</>
 				)}
+			</div>
+			<div className="d-flex justify-content-center">
+				<button
+					className="btn btn-warning btn-sm mb-4"
+					type="button"
+					onClick={() => {
+						setPro(!pro);
+					}}
+				>
+					Cambiar modo
+				</button>
 			</div>
 		</>
 	);
