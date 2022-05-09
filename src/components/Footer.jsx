fc5f5825159f8c53dc11f9fc13250ad3ae8d6f51
Jeff Free <jefffreelance3@gmail.com>
2022-05-08 22:31:27 -0300

    :lipstick: styles improved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 const Footer = () => {
 	return (
 		<div>
-			<footer className="bg-light text-center p-2 mt-auto text-black sticky-bottom">
+			<footer className="bg-dark text-center p-2 mt-auto text-black sticky-bottom">
 				<Link href="https://github.com/Jeffuy">
-					<a className="h3" href="https://github.com/Jeffuy">
+					<a className="h3 text-white" href="https://github.com/Jeffuy">
 						JeffUy 2021 ©
 					</a>
 				</Link>
--- src/components/quiz/SelectedQuestions.jsx
+++ src/components/quiz/SelectedQuestions.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { QuizContext } from '@context/QuizContext';
+import main from '@scripts/sendMail';
 
 const SelectedQuestions = () => {
 	const { titleChanger, imgList, score, showScore, questions, currentQuestionNumber, handleAnswerOptionClick, back } = useContext(QuizContext);
@@ -9,9 +10,13 @@ const SelectedQuestions = () => {
 				{showScore ? (
 					<>
 						<div className="app">
-							<div className="row">
-								<img alt="" className="img-fluid pb-4" src={titleChanger()} />
-								<img alt="" className="img-fluid" src={imgList[score]} />
+							<div className="col">
+								<div className="row">
+									<img alt="" className="result-title pb-4" src={titleChanger()} />
+								</div>
+								<div className="row">
+									<img alt="" className="result" src={imgList[score]} />
+								</div>
 							</div>
 						</div>
 						<div className="container-fluid mt-4">
@@ -20,9 +25,30 @@ const SelectedQuestions = () => {
 									<div className="text-center fs-2">
 										Tu puntaje fue de {score} sobre {questions.length}
 									</div>
-									<button className="btn btn-dark form-control mt-4 mb-4" onClick={() => back()}>
-										Volver
-									</button>
+									<div className="text-center">
+										<br />
+										<input id="username" placeholder="Ingrese tu nombre" type="text" />
+										<button
+											onClick={() => {
+												// Si hay token que no importa si pone o no el username
+												let username = document.getElementById('username').value;
+												console.log(username);
+												main();
+												// libraryFunctions.sendMail(
+												// 	username,
+												// 	score,
+												// 	"Grado del quiz"
+												// );
+											}}
+										>
+											Enviar
+										</button>
+									</div>
+									<div className="container-fluid flex">
+										<button className="btn btn-dark form-control mt-4 mb-4" onClick={() => back()}>
+											Volver
+										</button>
+									</div>
 								</div>
 							</div>
 						</div>
