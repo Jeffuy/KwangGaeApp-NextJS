6897e26e93d83bfa5bb6512f31a9badd939d413e
Jeff Free <jefffreelance3@gmail.com>
2022-05-10 23:36:23 -0300

    :alembic: nodemailer test

--- src/pages/api/mails.js
+++ src/pages/api/mails.js
@@ -9,8 +9,6 @@ export default function async(req, res) {
 		const transport = nodemailer.createTransport({
 			host: process.env.MAIL_HOST,
 			port: process.env.MAIL_PORT,
-			secure: true,
-			debug: true,
 			auth: {
 				user: process.env.MAIL_USER,
 				pass: process.env.MAIL_PASS,
