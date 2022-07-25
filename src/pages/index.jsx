4bb6892fa782d19f2960bfadc0ef2fc8e4c56bd7
Jeff Free <jefffreelance3@gmail.com>
2022-07-24 21:10:47 -0300

    :lipstick: styles improved

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,21 +7,21 @@ const Home = () => {
 	return (
 		<>
 			<NextSeo description="Sitio web para alumnos, instructores y arbítros de Taekwon-Do I.T.F. Útil para torneos, clases y examenes" title="KwangGae App" />
-			<div>
-				<div className="container">
-					<header className="row">
-						<div className="col-lg-12 text-center mt-3 mb-3">
-							<Image alt="kwang-gae" className="animate__animated animate__bounceIn" height={275} src={logo} width={275} />
-						</div>
-					</header>
+			<div className="home">
+				<div className="home__logo-container">
+					<Image alt="kwang-gae" className="home__logo-container--img" height={250} src={logo} width={250} />
 				</div>
-				<div className="container-fluid">
-					<div className="row bg-dark text-white p-5 text-center">
-						<div className="col-sm-6">
-							<i className="fas fa-mobile-alt fa-5x" />
-						</div>
-						<div className="col-sm-6 pt-4">
+
+				<div className="home__description-container">
+					<div className="home__description-container--icon">
+						<i className="fas fa-mobile-alt fa-5x" />
+					</div>
+					<div className="home__description-container--text">
+						<div className="home__description-container--title">
 							<h3>La aplicación de Kwang-Gae</h3>
+						</div>
+
+						<div className="home__description-container--description">
 							<p>La mejor herramienta para mejorar tus clases de Taekwon-Do. Arbitraje, selector de formas, desafíos, información y videos. Desarrollado por profesores y alumnos</p>
 						</div>
 					</div>
