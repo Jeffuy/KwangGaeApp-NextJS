d833f1cf1392819b1a9b986a06bedae09d41aab7
Jeff Free <jefffreelance3@gmail.com>
2022-07-26 00:17:39 -0300

    :lipstick: pattern selector styles improved

--- src/components/selector/SelectorResults.jsx
+++ src/components/selector/SelectorResults.jsx
@@ -1,24 +1,14 @@
-import React, { useContext } from "react";
-import { PatternSelectorContext } from "@context/PatternSelectorContext";
+import React, { useContext } from 'react';
+import { PatternSelectorContext } from '@context/PatternSelectorContext';
 
 const SelectorResults = () => {
-
 	const { formas } = useContext(PatternSelectorContext);
 
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <p
-                        className="text-center fs-1 mt-5 mb-5 width-special"
-                        id="result"
-                    >
-                        {formas}
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="pattern-selector__result-container">
+			<p id="result">{formas}</p>
+		</div>
+	);
 };
 
 export default SelectorResults;
