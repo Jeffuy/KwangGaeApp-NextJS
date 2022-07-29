27f0679ba6ca0e99e1ae4c237dec6947da9ee0c2
Jeff Free <jefffreelance3@gmail.com>
2022-07-28 22:34:39 -0300

    :pencil: challenges typos

--- src/components/challenges/ChallengeItem.jsx
+++ src/components/challenges/ChallengeItem.jsx
@@ -10,7 +10,7 @@ const ChallengeItem = props => {
 			>
 				<i className={`Icon Icon-check fas fa-check-circle fa-2x ${props.completed && 'Icon-check--active'}`} onClick={props.onComplete} />
 				<p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
-					Haz <b>{props.text}</b> para sumar <b>{props.points} puntos.</b>
+					Haz <b>{props.text}</b>. Recompensa: <b>{props.points} puntos.</b>
 				</p>
 
 				<i className="fas fa-trash-alt fa-2x Icon-delete" onClick={props.onDelete} />
