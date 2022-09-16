d90475348516c8a2c5046ff8a5c8532b5af7612c
Jeff Free <jefffreelance3@gmail.com>
2022-09-16 00:04:09 -0300

    buttons style reworked

--- src/components/fight/FightTime.jsx
+++ src/components/fight/FightTime.jsx
@@ -5,7 +5,7 @@ const FightTime = () => {
 	const { time } = useContext(FightContext);
 
 	return (
-		<div className="d-flex justify-content-center mb-2 h2">
+		<div className="fight-timer">
 			<span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
 			<span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
 			<span>{('0' + ((time / 10) % 100)).slice(-2)}</span>
