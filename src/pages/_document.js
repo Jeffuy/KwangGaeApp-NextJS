8b6de95893fafbcdbd98bbb9cb58936d7d80af3a
Jeff Free <jefffreelance3@gmail.com>
2022-09-17 02:37:45 -0300

    site.webmanifest commented

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -45,7 +45,7 @@ class MyDocument extends Document {
 					<link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
 					<link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
 					<link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-					<link href="/site.webmanifest" rel="manifest" />
+					{/* <link href="/site.webmanifest" rel="manifest" /> */}
 					<meta content="#da532c" name="msapplication-TileColor" />
 					<meta content="#ffffff" name="theme-color" />
 				</Head>
