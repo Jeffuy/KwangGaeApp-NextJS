de1bac332f913fd806e5aedf29fea71d9e50deeb
Jeff Free <jefffreelance3@gmail.com>
2022-12-08 01:11:43 -0300

    new pack corrected

--- src/components/market/NewPack.jsx
+++ src/components/market/NewPack.jsx
@@ -16,15 +16,15 @@ const NewPack = ({ newPack }) => {
 				{newPack.map(sticker => (
 					<div key={sticker.number} className="album-map-container">
 						<div
-							className={`${sticker.vertical ? 'new-sticker-vertical' : 'new-sticker'} ${sticker.circular ? 'album-sticker-circular' : ''}`}
+							className={`${sticker.vertical ? 'new-sticker-vertical' : 'new-sticker'} ${sticker.circular ? 'new-sticker-circular' : ''}`}
 							onClick={() => setClicked([...clicked, sticker.number])}
 						>
 							<p className={clicked.includes(sticker.number) ? 'album-sticker-number-clicked' : 'album-sticker-number'}>{clicked.includes(sticker.number) ? sticker.number : '?'}</p>
-							<Image alt={sticker.number} className={clicked.includes(sticker.number) ? 'album-image-clicked' : 'album-image-not-clicked'} layout="fill" src={sticker.url} />
+							<Image alt={sticker.number} className={clicked.includes(sticker.number) ? 'new-sticker-clicked' : 'new-sticker-not-clicked'} layout="fill" src={sticker.url} />
 							<Image
 								priority
 								alt={sticker.number}
-								className={clicked.includes(sticker.number) ? 'album-image-clicked-back' : 'album-image-not-clicked-back'}
+								className={clicked.includes(sticker.number) ? 'new-sticker-clicked-back' : 'new-sticker-not-clicked-back'}
 								layout="fill"
 								src={`${sticker.circular ? 'https://i.imgur.com/GDWAqoq.png' : 'https://i.imgur.com/rwYWQuA.png'} `}
 							/>
