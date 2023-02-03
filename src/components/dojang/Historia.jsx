dafe1004837f59eea1d111dab308eb96f94dd7fb
Jeff Free <jefffreelance3@gmail.com>
2023-02-03 20:47:58 -0300

    fixed dependencies

--- src/components/dojang/Historia.jsx
+++ src/components/dojang/Historia.jsx
@@ -1,5 +1,4 @@
 
-import Image from 'next/image';
 import { NextSeo } from 'next-seo';
 
 const Historia = () => {
--- src/components/dojang/Significado.jsx
+++ src/components/dojang/Significado.jsx
@@ -1,5 +1,5 @@
 
-import Image from 'next/image';
+
 import { NextSeo } from 'next-seo';
 
 const Significado = () => {
