b182054c528e92b09442cf5960294165d1f30f76
Jeff Free <jefffreelance3@gmail.com>
2024-01-28 15:39:44 -0300

    sonner deleted

--- src/components/selector/SelectorChooseButton.jsx
+++ src/components/selector/SelectorChooseButton.jsx
@@ -1,20 +1,20 @@
 import React, { useContext } from 'react';
 import { PatternSelectorContext } from '@context/PatternSelectorContext';
-import { Toaster, toast } from 'sonner';
+// import { Toaster, toast } from 'sonner';
 
 const SelectorChooseButton = () => {
 	const { elegirFormas } = useContext(PatternSelectorContext);
 
-	const handleElegirFormas = () => {
-		toast(elegirFormas());
-	};
+	// const handleElegirFormas = () => {
+	// 	toast(elegirFormas());
+	// };
 
 	return (
 		<div className="pattern-selector__button-container">
-			<button className="" type="button" onClick={handleElegirFormas}>
+			<button className="" type="button" onClick={elegirFormas}>
 				Elige las formas
 			</button>
-			<Toaster />
+			{/* <Toaster /> */}
 		</div>
 	);
 };
